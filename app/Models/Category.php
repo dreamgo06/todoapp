899100dec7386723bfd7e998f4d59926ee51e00d
Thiranai Boonsilp <thiranai.boonsilp06@gmail.com>
2021-06-13 16:00:36 +0700

    เพิ่ม

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
-    public function posts(){
+    public function activities(){
         return $this->hasMany(Activity::class);
       }
 
